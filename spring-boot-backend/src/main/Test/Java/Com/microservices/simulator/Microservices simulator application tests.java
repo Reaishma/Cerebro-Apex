d00053e66b452a41ec731d98ab79870840b014de
Reaishma N <hayagreevar9018@gmail.com>
2025-07-18 16:16:23 +0530

    Update and rename spring-boot-backend/src/main/java/com/microservices/simulator/MicroservicesSimulatorApplication.java to spring-boot-backend/src/main/Test/Java/Com/microservices/simulator/Microservices simulator application tests.java

---
+++ spring-boot-backend/src/main/Test/Java/Com/microservices/simulator/Microservices simulator application tests.java
@@ -0,0 +1,12 @@
+ package com.microservices.simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
